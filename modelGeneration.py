4371e3664d1d9be8c62999275c3284d50d2ad1fb
Jeff Kaplan <jeffkaplan@caltech.edu>
2012-12-05 10:34:01 -0800

    Started work on modelGeneration

--- modelGeneration.py
+++ modelGeneration.py
@@ -5,5 +5,13 @@ __author__ = 'jeff'
 
 
 class modelGenerator():
+    rotNS_location=""
+    makeEOSfile_location=""
     requestQueue=[]
+    def __init__(self,rotNS_location,makeEOSfile_location):
+        self.rotNS_location=rotNS_location
+        self.makeEOSfile_location=makeEOSfile_location
 
+
+    def runModel(self, inputParams):
+        runParams, eosParams=inputParams
