3806e61c37b53aae1f34e761eaec777c6493ee4b
Jeff Kaplan <jeffkaplan@caltech.edu>
2012-12-19 12:52:06 -0800

    Added real main function.

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Template for main function
+
+Jeff Kaplan <jeffkaplan@caltech.edu>
+"""
+import sys
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
