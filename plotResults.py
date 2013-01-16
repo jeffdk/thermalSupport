37390930c3f60afba679cafbe808541bf1da2a68
Jeff Kaplan <jeffkaplan@caltech.edu>
2013-01-16 09:16:46 -0800

    Added tov models and 3D option for plotResults

--- plotResults.py
+++ plotResults.py
@@ -2,7 +2,7 @@
 import sys
 import sqlite3
 import matplotlib.pyplot as mpl
-#from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D
 import numpy
 from sqlUtils import queryDBGivenParams
 
@@ -21,9 +21,15 @@ c=connection.cursor()
 connection.close()
 del connection
 
+databaseFile = '/home/jeff/work/rotNSruns/hotTov.db'
+connectionTov=sqlite3.connect(databaseFile)
+cTov=connectionTov.cursor()
+
+sequencePlot(["edMax","baryMass"],cTov,(),"T",grid=True,title="TOV Models for Shen_14.0_0.5")
 
 #Find energy density of maximum mass as function of variables
-
+plotVar = 'edMax'
+plot3d  = True
 def edOfMaxMass(filters,connection):
     c=connection.cursor()
     answer={'max':-1.e10,'edMax':0.0,'RedMax':False}
@@ -34,6 +40,14 @@ def edOfMaxMass(filters,connection):
             answer['edMax']=edMax
             answer['RedMax']=RedMax
     return answer
+
+
+resultTov=[]
+for T in cTov.execute("SELECT DISTINCT T FROM models"):
+    T=T[0]
+    answer = edOfMaxMass( ("T>%s" % (T -.01), "T<%s" % (T +.01))  ,connectionTov)
+    resultTov.append((-.10,T,answer[plotVar]))
+
 
 
 databaseFile         = '/home/jeff/work/rotNSruns/mass-shed-models.db'
@@ -56,7 +70,7 @@ for a,T in grid:
     print a,T
     answer = edOfMaxMass( ("a>%s" % (a-.001), "a<%s" % (a+.001),
                            "T>%s" % (T -.01), "T<%s" % (T +.01))  ,connection)
-    plotVar = 'edMax'
+
     if answer['RedMax'] > 0:
         resultT.append((a,T,answer[plotVar]))
     else:
@@ -68,15 +82,37 @@ for a,T in grid:
 
 resultS=zip(*resultS)
 resultT=zip(*resultT)
+resultTov=zip(*resultTov)
 
-#fig=mpl.figure()
-#ax = fig.add_subplot(111)
+if plot3d:
+    fig=mpl.figure()
+    ax = fig.add_subplot(111,projection='3d')
+    print resultS
+    ax.scatter(resultS[0],resultS[1],resultS[2],color='b',marker='o',label="Spheroid")
+    ax.scatter(*resultT,color='r',marker='^', label="toroid")
+    ax.scatter(*resultTov,color='g',marker='s', label="tov")
+    ax.set_xlabel("Differential rotation parameter A (-.1 is TOV)")
+    ax.set_ylabel("Temperature (MeV)")
+    if plotVar=="edMax":
+        ax.set_zlabel("Maximum energy density of model with largest mass")
+    if plotVar=="max":
+        ax.set_zlabel("Gravitational mass of model with largest mass")
+    handles,labels = ax.get_legend_handles_labels()
+    #mpl.legend(["Spheroid","Toroid","TOV"])
+
+    mpl.show()
+    exit()
+
 mpl.scatter(resultS[0],resultS[2],c=resultS[1],s=40,marker='o')
 mpl.scatter(resultT[0],resultT[2],c=resultT[1],s=40,marker='^')
+mpl.scatter(resultTov[0],resultTov[2],c=resultTov[1],s=40,marker='s')
 colorLegend=mpl.colorbar()
 colorLegend.set_label("Model Temperature (MeV)")
 mpl.grid(True)
 mpl.xlabel("Differential rotation parameter A")
-mpl.ylabel("Maximum energy density of model with largest mass")
+if plotVar=="edMax":
+    mpl.ylabel("Maximum energy density of model with largest mass")
+if plotVar=="max":
+    mpl.ylabel("Gravitational mass of model with largest mass")
 mpl.title("Properties of model with largest mass Shen_14_0.5")
 mpl.show()
