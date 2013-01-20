542a836cd502986b476c1f8a06d3c925f7c7bf58
Jeff Kaplan <jeffkaplan@caltech.edu>
2013-01-19 16:25:58 -0800

    Add manualParse utility file.

---
+++ manualParse.py
@@ -0,0 +1,44 @@
+
+
+import glob
+import os
+import sqlite3
+from parseFiles import *
+
+
+databaseFile         = '/home/jeff/work/rotNSruns/manualParse.db'
+connection=sqlite3.connect(databaseFile)
+c=connection.cursor()
+#c.execute("CREATE TABLE models" + columnsString)
+currentDirectory=os.getcwd()
+os.chdir("/home/jeff/work/rotNSruns/")
+
+directoryBase="5651511.*"
+eosBase="HS"
+nonOutputRunParameters=[{}]
+nonOutputRunParameters[0]['eos']=eosBase
+nonOutputRunParameters[0]['rollMid']=14.0
+nonOutputRunParameters[0]['rollScale']=0.5
+nonOutputRunParameters[0]['T']=0.5
+
+
+dirNames=glob.glob(directoryBase)
+
+for thisDir in dirNames:
+    file = glob.glob(thisDir +"/"+ eosBase + "*")
+
+    #We must get a from the parameters file
+    paramsFile=open( thisDir +"/"+ "Parameters.input")
+    for line in paramsFile.readlines():
+        if "Rotfunc" in line:
+            a=float(line.split(":")[1])
+    print "A is: ", a
+    nonOutputRunParameters[0]['a']=a
+
+    entries = parseCstFileList(file,nonOutputRunParameters)
+
+    parseEntriesIntoDB(entries,c,"models",3,thisDir)
+
+    print file
+
+connection.close()
