31c5b3f926e515989816f828ffce07db47fdd055
Jeff Kaplan <jeffkaplan@caltech.edu>
2013-03-22 19:08:01 -0700

    Add huge gross manyScriptSequencePlot function. -It works, though

--- scriptPlotstyleDatabase.py
+++ scriptPlotstyleDatabase.py
@@ -28,7 +28,7 @@ def getScriptsDb():
     Database is a list of eosPrescription objects
     """
     database = []
-    nonesDict = dict([(key, None) for key in eosPrescription.prescriptionParameters])
+    nonesDict = dict([(key, str(None)) for key in eosPrescription.prescriptionParameters])
 
     plateau10 = eosPrescription("Plateau 10 MeV", 'o', '-')
     plateau10.setParameters(nonesDict)
@@ -40,7 +40,7 @@ def getScriptsDb():
     plateau5.setParameters(plateau5dict)
     database.append(plateau5)
 
-    rolloff_m14_s05_T40 = eosPrescription("Rolloff mid=14 scale=.5  T=40", '^', ':')
+    rolloff_m14_s05_T40 = eosPrescription(r"$\mathrm{Roll}^{\mathrm{mid}=14}_{\mathrm{scale}=.5}$ T=40", '^', ':')
     rolloff_m14_s05_T40dict = nonesDict.copy()
     rolloff_m14_s05_T40dict.update({'T': 40,
                                     'rollMid': 14.0,
@@ -49,7 +49,7 @@ def getScriptsDb():
     rolloff_m14_s05_T40.setParameters(rolloff_m14_s05_T40dict)
     database.append(rolloff_m14_s05_T40)
 
-    rolloff_m14_s05_T20 = eosPrescription("Rolloff mid=14 scale=.5  T=20", 's', '-.')
+    rolloff_m14_s05_T20 = eosPrescription("$\mathrm{Roll}^{\mathrm{mid}=14}_{\mathrm{scale}=.5}$  T=20", 'v', '-.')
     rolloff_m14_s05_T20dict = nonesDict.copy()
     rolloff_m14_s05_T20dict.update({'T': 20,
                                     'rollMid': 14.0,
@@ -68,6 +68,72 @@ def getScriptsDb():
     database.append(cold)
 
 
+    rolloff_m135_s05_T20 = eosPrescription("Roll mid=13.5 scale=.5  T=20", '1', '-.')
+    rolloff_m135_s05_T20dict = nonesDict.copy()
+    rolloff_m135_s05_T20dict.update({'T': 20,
+                                    'rollMid': 13.5,
+                                    'rollScale': 0.5,
+                                    'eosTmin': 0.01})
+    rolloff_m135_s05_T20.setParameters(rolloff_m135_s05_T20dict)
+    database.append(rolloff_m135_s05_T20)
+
+    rolloff_m135_s05_T40 = eosPrescription("Roll mid=13.5 scale=.5  T=40", '2', '-.')
+    rolloff_m135_s05_T40dict = nonesDict.copy()
+    rolloff_m135_s05_T40dict.update({'T': 40,
+                                    'rollMid': 13.5,
+                                    'rollScale': 0.5,
+                                    'eosTmin': 0.01})
+    rolloff_m135_s05_T40.setParameters(rolloff_m135_s05_T40dict)
+    database.append(rolloff_m135_s05_T40)
+
+    rolloff_m14_s025_T40 = eosPrescription("Roll mid=14 scale=.25  T=40", '3', ':')
+    rolloff_m14_s025_T40dict = nonesDict.copy()
+    rolloff_m14_s025_T40dict.update({'T': 40,
+                                    'rollMid': 14.0,
+                                    'rollScale': 0.25,
+                                    'eosTmin': 0.01})
+    rolloff_m14_s025_T40.setParameters(rolloff_m14_s025_T40dict)
+    database.append(rolloff_m14_s025_T40)
+
+    rolloff_m14_s025_T20 = eosPrescription("Roll mid=14 scale=.25  T=20", '4', '-.')
+    rolloff_m14_s025_T20dict = nonesDict.copy()
+    rolloff_m14_s025_T20dict.update({'T': 20,
+                                    'rollMid': 14.0,
+                                    'rollScale': 0.25,
+                                    'eosTmin': 0.01})
+    rolloff_m14_s025_T20.setParameters(rolloff_m14_s025_T20dict)
+    database.append(rolloff_m14_s025_T20)
+
+
+    entropyS05 = eosPrescription("Fixed Entropy S = 0.5", "_", '-')
+    entropyS05dict = nonesDict.copy()
+    entropyS05dict.update({'fixedQuantity': 'entropy',
+                           'fixedTarget': 0.5})
+    entropyS05.setParameters(entropyS05dict)
+    database.append(entropyS05)
+
+    entropyS1 = eosPrescription("Fixed Entropy S = 1.0", "|", '-')
+    entropyS1dict = nonesDict.copy()
+    entropyS1dict.update({'fixedQuantity': 'entropy',
+                          'fixedTarget': 1.0})
+    entropyS1.setParameters(entropyS1dict)
+    database.append(entropyS1)
+
+    entropyS2 = eosPrescription("Fixed Entropy S = 2.0", "d", '-')
+    entropyS2dict = nonesDict.copy()
+    entropyS2dict.update({'fixedQuantity': 'entropy',
+                          'fixedTarget': 2.0})
+    entropyS2.setParameters(entropyS2dict)
+    database.append(entropyS2)
+
+    entropyS3 = eosPrescription("Fixed Entropy S = 3.0", "D", '-')
+    entropyS3dict = nonesDict.copy()
+    entropyS3dict.update({'fixedQuantity': 'entropy',
+                          'fixedTarget': 3.0})
+    entropyS3.setParameters(entropyS3dict)
+    database.append(entropyS3)
+
+
     #check every script in the database is set fully
     for script in database:
         assert isinstance(script, eosPrescription), \
@@ -84,58 +150,153 @@ def symbolFromDBentry(paramsDict):
     database = getScriptsDb()
 
     symbol = None
+    name = None
 
     for script in database:
+        #print paramsDict
+        #print script.paramsDict
         if paramsDict == script.paramsDict:
-            print "Match for: ", script.name
+            #print "Match for: ", script.name
             symbol = script.symbol
-
+            name = script.name
     assert symbol is not None, "Couldn't find entry matching paramsDict!"
-    return symbol
+    return symbol, name
 
-def manyScriptSequencePlot(plotFields, sqliteCursor, filters=(), colorBy=None,
+def getLinestyle(eos, ye):
+    """
+    Defines database for what linestyle to plot with
+    """
+    if eos == 'HShenEOS' and ye == .15:
+        return '-'
+    if eos == 'HShenEOS' and ye == 'BetaEq':
+        return '--'
+    if eos == 'LS220' and ye == .15:
+        return ':'
+    if eos == 'LS220' and ye == 'BetaEq':
+        return '-.'
+
+
+def manyScriptSequencePlot(plotFields, sqliteCursor, filters=(), colorBy=None, maxPlot='gravMass',
                            grid=True, title="", orderBy=None, forceColorbar=False,
+                           suppressShow=False, legendList=[], vmax=None, vmin=None,
                            **mplKwargs):
     """
     Version of sequence plot for plotting huge amount of data
     assumes tablename is models
-
+    if maxPlot plots maximum value of quantity for sequence only
     """
-    filters = ("ye=0.15",)
+    orderBy = plotFields[0] + ',' + ','.join(eosPrescription.prescriptionParameters) + ",eos,ye,edMax"
+    #print orderBy
+    size = 80
+    plotLines = True
+    #filters = ("ye=0.15",)
+    filters += ("arealR < 100",)
     tableName = "models"
     if colorBy is None:
         colorBy = "arealR"
-    getFields = plotFields[:]
-    getFields.append(colorBy)
+    getFields = plotFields[:]  # x-points 0, y-points 1
+    getFields.append(colorBy)  # colorBy 2
     if orderBy is None:
         orderBy = plotFields[0]
 
     for key in eosPrescription.prescriptionParameters:
-        getFields.append(key)
-    getFields.append('RedMax')
+        getFields.append(key)   # script parameters 3-8
+    getFields.append('RedMax')  # RedMax 9
+    getFields.append('ye')      # ye 10
+    getFields.append('eos')     # eos 11
+    if maxPlot is not None:
+        getFields.append(maxPlot)  # maxPlot 12
 
     points = queryDBGivenParams(getFields, [], sqliteCursor,
                                 tableName, filters, " ORDER BY " + orderBy)
-    for i, point in enumerate(points):
-        prescriptDict = None
+    currentSymbol = ""
+    previousSymbol = ""
+    previousName = ""
+    currentName = ""
+    currentPoints = []
+    currentToroidPoints = []
+    currentEos = None
+    currentYe = None
+    previousEos = None
+    previousYe = None
+    if vmax is None:
+        vmax = max([point[2] for point in points])
+    if vmin is None:
+        vmin = min([point[2] for point in points])
 
-    points = zip(*points)
-    points = numpy.array(points)
-    print points[:, 3:]
-    #fig = plt.figure()
-    #axis = fig.add_subplot(111)
+    #print vmin, vmax
+    for i, point in enumerate(points):
+        currentYe = point[10]
+        currentEos = point[11]
+        prescriptDict = dict()
+        #print currentSymbol, previousSymbol
+        for j, scriptParam in enumerate(point[3:3 + 6 ]):
+            prescriptDict[eosPrescription.prescriptionParameters[j]] = scriptParam
+        #print prescriptDict
+        currentSymbol, currentName = symbolFromDBentry(prescriptDict)
+        ax = plt.gca()
+        handles, labels = ax.get_legend_handles_labels()
+        #print currentName, labels
+        if currentName in labels:
+            currentName = None
+            previousName = None
+        if currentSymbol == previousSymbol and currentEos == previousEos and currentYe == previousYe:
+            currentPoints.append(point)
+            if point[9] > 0.0:
+                #print "TOROIZZLE"
+                currentToroidPoints.append(point)
+        elif not previousSymbol == "":
+            if maxPlot is not None:
+                currentPoints = [getPointWithMaxOfCol(currentPoints, 12)]
+            currentPoints = zip(*currentPoints)
+            plt.scatter(*currentPoints[:2], c=currentPoints[2], marker=previousSymbol, label=previousName,
+                        vmax=vmax, vmin=vmin, s=size, **mplKwargs)
+            if plotLines:
+                plt.plot(*currentPoints[:2], ls=getLinestyle(previousEos, previousYe), c='k', lw=.5)
+            currentPoints = []
+            #legendList.append(currentName)
+            if not currentToroidPoints == []:
+                if maxPlot is not None:
+                    currentToroidPoints = [getPointWithMaxOfCol(currentToroidPoints, 12)]
+                    print currentToroidPoints
+                currentToroidPoints = zip(*currentToroidPoints)
+
+                plt.scatter(*currentToroidPoints[:2], c=currentToroidPoints[2],
+                            marker='o', facecolor='none', s=size * 1.5,
+                            vmax=vmax, vmin=vmin, **mplKwargs)
+                currentToroidPoints = []
+        previousSymbol = currentSymbol
+        previousEos = currentEos
+        previousYe = currentYe
+        previousName = currentName
+        #symbolList.append(symbolFromDBentry(prescriptDict))
+    #Don't forget to plot the last set!
+    print getPointWithMaxOfCol(currentPoints, 12)
+    if maxPlot is not None:
+        currentPoints = [getPointWithMaxOfCol(currentPoints, 12)]
+    currentPoints = zip(*currentPoints)
+    plt.scatter(*currentPoints[:2], c=currentPoints[2], marker=previousSymbol, label=previousName,
+                vmax=vmax, vmin=vmin, s=size, **mplKwargs)
+    if plotLines:
+        plt.plot(*currentPoints[:2], ls=getLinestyle(previousEos, previousYe), c='k', lw=.5)
+    #legendList.append(currentName)
+    if not currentToroidPoints == []:
+        if maxPlot is not None:
+            currentToroidPoints = [getPointWithMaxOfCol(currentToroidPoints, 12)]
+        currentToroidPoints = zip(*currentToroidPoints)
+        plt.scatter(*currentToroidPoints[:2], c=currentToroidPoints[2],
+                    marker='o', facecolor='none', s=size * 1.5,
+                    vmax=vmax, vmin=vmin, **mplKwargs)
+
+
     plt.title(title)
     plt.grid(grid)
-    if colorBy:
-        plt.scatter(*points[:2], c=points[2], **mplKwargs)
-    else:
-        plt.plot(*points[:2], **mplKwargs)
 
     #axis.set_xlabel(getFields[0])
     #axis.set_ylabel(getFields[1])
     plt.xlabel(getFields[0])
     plt.ylabel(getFields[1])
-    if colorBy:
+    if colorBy and not suppressShow:
         colorLegend = plt.colorbar()
         colorLegend.set_label(colorBy)
     if forceColorbar:
@@ -143,4 +304,23 @@ def manyScriptSequencePlot(plotFields, sqliteCursor, filters=(), colorBy=None,
         colorLegend.set_label(colorBy)
     print "Plotting %i entries" % len(points)
 
-    plt.show()
+    if not suppressShow:
+        #ax = plt.gca()
+        #handles, labels = ax.get_legend_handles_labels()
+        plt.legend()#handles, labels)
+        plt.show()
+        legendList = []
+
+def getPointWithMaxOfCol(array, col):
+    assert len(array), "Empty array passed!"
+    assert len(array[0]) > col, "Points don't have %s entries in array!"  % col
+
+    max = -1e300
+    answerPoint = None
+    for point in array:
+        if point[col] > max:
+            max = point[col]
+            answerPoint = point
+
+    assert answerPoint is not None, "Answer is None... something is wrong"
+    return answerPoint
--- sortPrescriptPlot3-20-13.py
+++ sortPrescriptPlot3-20-13.py
@@ -1,40 +1,40 @@
 import sys
 import sqlite3
-import matplotlib.pyplot as mpl
+from matplotlib import rcParams
 from scriptPlotstyleDatabase import symbolFromDBentry, manyScriptSequencePlot
 from sqlUtils import queryDBGivenParams
 
-
+rcParams['legend.frameon'] = False
 
 sys.path.append('./maxMassOrigFiles/')
 #noinspection PyUnresolvedReferences
 from sqlPlotRoutines import sequencePlot, equalsFiltersFromDict
 
 
-databaseFile = '/home/jeff/work/rotNSruns/allRuns3-20-13.db'
+databaseFile = '/home/jeff/work/rotNSruns/allRuns3-22-13.db'
 connectionTov=sqlite3.connect(databaseFile)
 c=connectionTov.cursor()
 #
 prescriptionParameters = ('T', 'rollMid', 'rollScale', 'eosTmin', 'fixedTarget', 'fixedQuantity')
 
-paramsDict = dict([(key, None) for key in prescriptionParameters])
+paramsDict = dict([(key, str(None)) for key in prescriptionParameters])
 #paramsDict['eosTmin'] = 5.0
 symbolFromDBentry(paramsDict)
-manyScriptSequencePlot(["edMax", "gravMass"], c, (), grid=True,  title="")
+manyScriptSequencePlot(["a", "edMax"], c, ("eos='HShenEOS'", "fixedQuantity='None'", "ye=.15"), "arealR",
+                       grid=True,  title="", vmax=40, vmin=12)
 
-exit()
+#exit()
 
 paramsDict['eos'] = 'HShenEOS'
 paramsDict['ye'] = 0.15
-paramsDict['edMax'] = 704166666600000.0
-paramsDict['a'] = 0.6
+#paramsDict['edMax'] = 704166666600000.0
+paramsDict['a'] = 1.0
 #paramsDict['rpoe'] = 0.7272727272727
-colorVar = "omega_c"
+colorVar = "arealR"
 xaxisVar = "rpoe"
-yaxisVar = "gravMass"
-#paramsDict['RedMax'] = 0.0
-vmax = 10000
-vmin = 0.0
+yaxisVar = "gravMass"#"MAX(gravMass)"
+vmax = 25
+vmin = 10.0
 
 # c.execute("SELECT DISTINCT edMax FROM models WHERE edMax < 7.3e14 AND edMax > 5.8e14")
 # grid = c.fetchall()
@@ -44,11 +44,11 @@ vmin = 0.0
 #     paramsDict['edMax'] = ed[0]
 #     filters = equalsFiltersFromDict(paramsDict,1e-1) #+ ("arealR<20",)
 #     print filters
-#     sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=True, title="",
+#     manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=True, title="",
 #                  s=50, vmax=vmax, vmin=vmin)
 #
 #
-# sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=True, title="",
+# manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=True, title="",
 #                  s=50, vmax=vmax, vmin=vmin ) #,vmax=3e15, vmin=1e14)
 #
 #
@@ -57,7 +57,7 @@ vmin = 0.0
 # #filters += ("arealR<60",)
 #
 #
-# #sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=True, title="", s=50, vmin=14, vmax=30)
+# #manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=True, title="", s=50, vmin=14, vmax=30)
 #
 # #paramsDict['ye'] = 'BetaEq'
 # #del paramsDict['fixedTarget']
@@ -72,31 +72,39 @@ vmin = 0.0
 #     paramsDict['edMax'] = ed[0]
 #     filters = equalsFiltersFromDict(paramsDict, 1e-1)
 #
-#     sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, suppressShow=True, grid=True, marker='^',
+#     manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, suppressShow=True, grid=True, marker='^',
 #                  vmin=vmin, vmax=vmax, s=50)
 #
-# sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=False, marker='^',
+# manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=False, marker='^',
 #              s=50, vmax=vmax, vmin=vmin, title="a = " + str(paramsDict['a']) ) #,vmax=3e15, vmin=1e14)
+filters = equalsFiltersFromDict(paramsDict, 1e-1)
+
+manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, vmin=vmin, vmax=vmax,
+                       grid=True, suppressShow=True, title="")
+
+paramsDict['eosTmin'] = 5.0
+
 filters = equalsFiltersFromDict(paramsDict)
 
-sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=True, title="",
-                 s=50, vmax=vmax, vmin=vmin ) #,vmax=3e15, vmin=1e14)
+manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, vmin=vmin, vmax=vmax,
+                       suppressShow=True, title="")
+
 
 paramsDict['T'] = 40.0
 paramsDict['rollMid'] = 14.0
 paramsDict['rollScale'] = 0.5
 paramsDict['eosTmin'] = 0.01
 #paramsDict['edMax'] = 0.7
-filters = equalsFiltersFromDict(paramsDict, 1e-1)
+filters = equalsFiltersFromDict(paramsDict)
 
-sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, suppressShow=True, grid=True, marker='^',
-                 vmin=vmin, vmax=vmax, s=50)
+manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, suppressShow=True, vmin=vmin, vmax=vmax,
+                       grid=True)
 
-sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=False, marker='^',
-             s=50, vmax=vmax, vmin=vmin, title="a = " + str(paramsDict['a']) ) #,vmax=3e15, vmin=1e14)
+#manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, suppressShow=False, marker='^',
+#             s=50, vmax=vmax, vmin=vmin, title="a = " + str(paramsDict['a']) ) #,vmax=3e15, vmin=1e14)
 
-exit()
-exit()
+
+
 paramsDict['T'] = 0.5
 paramsDict['rollMid'] = 14.0
 paramsDict['rollScale'] = 0.5
@@ -104,9 +112,8 @@ paramsDict['eosTmin'] = 0.01
 
 filters = equalsFiltersFromDict(paramsDict)
 
-sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, suppressShow=True, grid=True, marker='*',
-             vmin=vmin, vmax=vmax,
-             s=50)
+manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, suppressShow=True, vmin=vmin, vmax=vmax,
+                       grid=True)
 
 paramsDict['T'] = 20.0
 paramsDict['rollMid'] = 14.0
@@ -115,8 +122,7 @@ paramsDict['eosTmin'] = 0.01
 
 filters = equalsFiltersFromDict(paramsDict)
 
-sequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, marker='s',
-             s=50, vmin=vmin, vmax=vmax,
+manyScriptSequencePlot([xaxisVar, yaxisVar], c, filters, colorVar, grid=True, vmin=vmin, vmax=vmax,
              title="a = " + str(paramsDict['a']))
 
 
