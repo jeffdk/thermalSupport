7a8ec2a59b8c68548de7f649d65acc5fc22988fb
Jeff Kaplan <jeffkaplan@caltech.edu>
2013-04-05 19:19:39 -0700

    Full set of new plot facility methods working

--- datasetManager.py
+++ datasetManager.py
@@ -1,9 +1,9 @@
 import os
 import sqlite3
-import sys
+import numpy
 from maxMassOrigFiles.sqlPlotRoutines import equalsFiltersFromDict
 import parseFiles
-from scriptPlotstyleDatabase import tempPrescription, scriptFromScriptName
+from scriptPlotstyleDatabase import scriptFromScriptName
 from sqlUtils import queryDBGivenParams
 
 
@@ -63,12 +63,13 @@ class cstSequence(object):
 
     indVars = ('a', 'edMax', 'rpoe')
 
-    def __init__(self, inputCstDataset, fixedIndVarsDict):
+    #todo add support to remove duplicates
+    def __init__(self, inputCstDataset, fixedIndVarsDict, filters=()):
         """
         Takes fixedIndVarsDict, and sets the parameter to the indVar
         not in the dict.
+        filters only used for special case of selecting min rpoe
         """
-        #todo ADD MIN RPOE SUPPORT
         assert isinstance(inputCstDataset, cstDataset)
         assert all([key in self.indVars for key in fixedIndVarsDict.keys()]), \
             "Not all keys in fixedIndVarsDict are proper indVars"
@@ -76,24 +77,105 @@ class cstSequence(object):
 
         self.orderByVar = [indVar for indVar in self.indVars if indVar not in fixedIndVarsDict][0]
 
-        sliceFilters = equalsFiltersFromDict(fixedIndVarsDict, tolerance=1e-3)
+        copyOfInputDict = fixedIndVarsDict.copy()
+
+        #Handle special case of selecting rpoe min
+        restrictToRpoeMin = False
+        if 'rpoe' in copyOfInputDict:
+            if copyOfInputDict['rpoe'] == 'min':
+                #delete it so we filter out nothing; do the rpoe min filtering later
+                del copyOfInputDict['rpoe']
+                restrictToRpoeMin = True
+
+        sliceFilters = equalsFiltersFromDict(copyOfInputDict, tolerance=1e-3)
         query = "WHERE " + " AND ".join(sliceFilters) + " ORDER BY " + self.orderByVar
 
         self.dbConn = sqlite3.connect(':memory:')
         cursor = self.dbConn.cursor()
         cursor.execute("CREATE TABLE models " + parseFiles.columnsString)
-        print query
+
         cursor.execute("ATTACH '" + inputCstDataset.dbName + "' AS toMerge")
-        cursor.execute("INSERT INTO models SELECT * FROM toMerge.models " + query)
+        cursor.execute("INSERT INTO models SELECT DISTINCT * FROM toMerge.models " + query)
         self.dbConn.commit()
 
-        for i in cursor.execute("SELECT * FROM models"):
-            print i
+        if restrictToRpoeMin:
+            # min rpoe occurs at the max run number for a given runID
+            # method: for each runID, find the max lineNum (satisfying the filters!)
+            # then, for that runID, delete entries that don't have that lineNum
+            cursor.execute("SELECT DISTINCT runID FROM models")
+            runIDs = cursor.fetchall()
+            #print  len(runIDs), runIDs
+            filtersString = ""
+            if filters:
+                filtersString = " AND " + " AND ".join(filters)
+            #print filtersString
+            for runID in runIDs:
+                runID = runID[0]
+                cursor.execute("SELECT MAX(lineNum) FROM models WHERE runID='" + runID + "'"
+                               + filtersString)
+                maxLineNum = cursor.fetchone()[0]
+                #print runID, maxLineNum
+                cursor.execute("DELETE FROM models WHERE runID='" + runID
+                               + "' AND lineNum!=" + str(maxLineNum))
+
+        #for i in cursor.execute("SELECT * FROM models"):
+        #    print i
 
         cursor.execute("SELECT count(*) FROM models")
         print "Sliced %s entries into cstSequence '%s' from cstDataset '%s'"\
               % (cursor.fetchone()[0], str(fixedIndVarsDict), inputCstDataset.name)
 
     def getSeqPlot(self, xcolVars, ycolVars, filters=(), colorVars=None,
-                   xcolFunc=lambda x: x, ycolFunc=lambda x:x, colorVarsFunc=lambda x: x):
-        pass
+                   xcolFunc=lambda x: x, ycolFunc=lambda x: x, colorVarsFunc=lambda x: x):
+        """
+        Selects data from the sequence for use in plotting.
+        Will filter data if requested. Functions are for doing calculations with data.
+        Returns numpy array of [xcol, ycol] or if colorVars, [xcol, ycol, colorCol]
+        """
+        answer = None
+        if colorVars is not None:
+            xs = self.getColumnData(xcolVars, xcolFunc, filters)
+            ys = self.getColumnData(ycolVars, ycolFunc, filters)
+            cs = self.getColumnData(colorVars, colorVarsFunc, filters)
+            assert len(xs) == len(ys) and len(ys) == len(cs), \
+                "Uh oh, columns not equal length; something very bad has happened"
+            answer = numpy.array([xs, ys, cs])
+        else:
+            xs = self.getColumnData(xcolVars, xcolFunc, filters)
+            ys = self.getColumnData(ycolVars, ycolFunc, filters)
+            assert len(xs) == len(ys), \
+                "Uh oh, columns not equal length; something very bad has happened"
+            answer = numpy.array([xs, ys])
+        #print answer
+        return answer
+
+    def getColumnData(self, colVars, colFunction, filters=()):
+
+        rawData = queryDBGivenParams(colVars, [], self.dbConn.cursor(), 'models', filters,
+                                     " ORDER BY " + self.orderByVar)
+        #print rawData
+        # light todo: add try catch to check num args
+        rawData = [colFunction(*entry) for entry in rawData]
+        #print rawData
+        return rawData
+
+
+#todo add colorVar support or rethink this function
+def reduceTwoSeqPlots(seqA, seqB, func, tolerance=1e-5):
+    """
+    Takes two sets of output from getSeqPlot, and computes func on
+    the entries in the ycol.  Only adds entry if xcols are within tol of each other
+    ORDER MATTERS: x points used are seqA's x points!
+    """
+
+    newXs = []
+    newYs = []
+    for i, xpoint in enumerate(seqA[0]):
+        for j, xpointSeqB in enumerate(seqB[0]):
+            if abs(xpoint - xpointSeqB) < tolerance:
+                newXs.append(xpoint)
+                newYs.append(func(seqA[1][i], seqB[1][j]))
+                #found match, so break
+                break
+
+    return numpy.array([newXs, newYs])
--- testNewPlotFacility.py
+++ testNewPlotFacility.py
@@ -1,14 +1,34 @@
-
-from datasetManager import cstDataset, cstSequence
+import matplotlib.pyplot as plt
+from datasetManager import cstDataset, cstSequence, reduceTwoSeqPlots
 
 sourceDb = '/home/jeff/work/rotNSruns/allRuns3-25-13.db'
 
-c30p0 = cstDataset("c30p0", "HShenEOS", 0.1, sourceDb)
+c40p0 = cstDataset("c40p0", "HShenEOS", 0.1, sourceDb)
+c0p0 = cstDataset("cold", "HShenEOS", 0.1, sourceDb)
 
-thisSeq = cstSequence(c30p0, {'a': 0.0, 'rpoe': 1.0})
-print "dur"
-print "dur"
+sliceDict = {'a': 1.0, 'rpoe': 'min'}
+filters = ()  # ('ToverW<.12', 'RedMax=0.0')#('ToverW>0.1',)
 
-thisSeq = cstSequence(c30p0, {'a': 1.0, 'edMax': 323076923000000.})
+thisSeq40 = cstSequence(c40p0, sliceDict, filters)
 
-thisSeq = cstSequence(c30p0, {'rpoe': 1.0, 'edMax': 323076923000000.})
+thisSeq0 = cstSequence(c0p0, sliceDict, filters)
+
+#thisSeq.getColumnData(['J', 'gravMass'], lambda j, m: j / (m * m), ('ToverW<.25',))
+#
+# guy40 = thisSeq40.getSeqPlot(['rpoe'], ['J', 'gravMass'], filters,
+#                              ycolFunc=lambda j, m: j / (m * m))
+# guy0 = thisSeq0.getSeqPlot(['rpoe'], ['J', 'gravMass'], filters,
+#                            ycolFunc=lambda j, m: j / (m * m))
+guy40 = thisSeq40.getSeqPlot(['edMax'], ['gravMass'], filters,
+                             ycolFunc=lambda j: j)
+guy0 = thisSeq0.getSeqPlot(['edMax'], ['gravMass'], filters,
+                           ycolFunc=lambda j: j)
+result = reduceTwoSeqPlots(guy0, guy40, lambda a, b: b / a - 1.0)
+
+print result
+
+plt.plot(*result)
+plt.show()
+
+
+#thisSeq = cstSequence(c30p0, {'rpoe': 1.0, 'edMax': 323076923000000.})
