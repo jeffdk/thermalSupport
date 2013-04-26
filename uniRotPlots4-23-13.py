0e961da675b4945bb24ac4fcca4bccc4d716369f
Jeff Kaplan <jeffkaplan@caltech.edu>
2013-04-25 18:11:28 -0700

    Add inset and polish.

--- uniRotPlots4-23-13.py
+++ uniRotPlots4-23-13.py
@@ -13,12 +13,12 @@ myfig.subplots_adjust(top=0.967)
 myfig.subplots_adjust(right=0.97)
 
 sourceDb = '/home/jeff/work/rotNSruns/shedRuns4-24-13.db'
-
+tovSourceDb = '/home/jeff/work/rotNSruns/allRuns3-25-13.db'
 shenEosTableFilename = '/home/jeff/work/HShenEOS_rho220_temp180_ye65_version_1.1_20120817.h5'
 ls220EosTableFilename = '/home/jeff/work/LS220_234r_136t_50y_analmu_20091212_SVNr26.h5'
 
-eosName = 'LS220'
-theEos = eosDriver(ls220EosTableFilename)
+eosName = 'HShenEOS'
+theEos = eosDriver(shenEosTableFilename)
 ye = 'BetaEq'
 #yeForInversion = 0.1
 
@@ -56,41 +56,63 @@ tempFuncsDict = {scriptsList[i]: tempFuncs[i] for i in range(len(scriptsList))}
 #############################################################
 # First plot: Just Shen, frac diffs
 #############################################################
-filters = ('edMax>2e14', 'ToverW<0.25')
+filters = ('edMax>0.5e14',)
+plotList = []
 coldTovSet = cstDataset('cold', eosName, ye, sourceDb)
 coldTovSeq = cstSequence(coldTovSet, tovSlice, filters)
-theEos.resetCachedBetaEqYeVsRhobs(tempFuncsDict['cold'], 14.0, 16.0)
+theEos.resetCachedBetaEqYeVsRhobs(tempFuncsDict['cold'], 13.5, 16.0)
 coldTovPlot = \
     coldTovSeq.getSeqPlot([xVar], yVars, filters, \
       xcolFunc=lambda x: theEos.rhobFromEnergyDensityWithTofRho(x, ye, tempFuncsDict['cold']),
       ycolFunc=yFunc)
-plt.semilogx(*coldTovPlot, c=colors['cold'], ls='--',  label="TOV")
+plot = plt.semilogx(*coldTovPlot, c=colors['cold'], ls='--',  label="TOV")
 del coldTovSet
 for script in colors.keys():
     thisSet = cstDataset(script, eosName, ye, sourceDb)
     thisSeq = cstSequence(thisSet, uniformMaxRotSlice, filters)
 
-    theEos.resetCachedBetaEqYeVsRhobs(tempFuncsDict[script], 14.0, 16.0)
+    theEos.resetCachedBetaEqYeVsRhobs(tempFuncsDict[script], 13.5, 16.0)
 
     thisPlot = thisSeq.getSeqPlot([xVar], yVars, filters, \
       xcolFunc=lambda x: theEos.rhobFromEnergyDensityWithTofRho(x, ye, tempFuncsDict[script]),
       ycolFunc=yFunc)
 
-    plt.plot(*thisPlot, c=colors[script], marker=symbols[script],  label=script)
-    
-    #tovSet = cstDataset(script, eosName, ye, sourceDb)
-    # tovSeq = cstSequence(thisSet, tovSlice, filters)
-    # tovPlot = \
-    #   tovSeq.getSeqPlot([xVar], yVars, filters, \
-    #     xcolFunc=lambda x: theEos.rhobFromEnergyDensityWithTofRho(x, ye, tempFuncsDict[script]),
-    #     ycolFunc=yFunc)
-    # plt.plot(*tovPlot, c=colors[script], ls='--')
+    plt.plot(*thisPlot, c=colors[script],  label=script)
+    plotList.append([thisPlot, colors[script], '-'])
+    del thisSet
+    tovSet = cstDataset(script, eosName, ye, tovSourceDb)
+    tovSeq = cstSequence(tovSet, tovSlice, filters)
+    tovPlot = \
+      tovSeq.getSeqPlot([xVar], yVars, filters, \
+        xcolFunc=lambda x: theEos.rhobFromEnergyDensityWithTofRho(x, ye, tempFuncsDict[script]),
+        ycolFunc=yFunc)
+    plt.plot(*tovPlot, c=colors[script], ls='--')
+    plotList.append([tovPlot, colors[script], '--'])
+    del tovSet
 
 plt.xlabel(r"$\rho_{b,\mathrm{max}}$ [g/cm$^3$]")
 #plt.axes().yaxis.set_minor_formatter(matplotlib.pyplot.FormatStrFormatter('%.0f'))
 #plt.axes().yaxis.set_major_formatter(matplotlib.pyplot.FormatStrFormatter('%.0f'))
-plt.ylabel("$M_b/M_\odot$", labelpad=5)
+plt.ylabel("$M_g/M_\odot$", labelpad=5)
 #removeExponentialNotationOnAxis('y')
 plt.legend(loc=2)
-plt.text(10 **15.1, 1.6, eosName, fontsize=26)
+plt.xlim([2.25e14, 2.05e15])
+#plt.ylim([0.0, 2.5])  # Mg LS220
+plt.ylim([0.0, 2.85])   # Mb LS220
+plt.ylim([0.0, 3.07])   # Mb shen
+plt.ylim([0.0, 2.7])   # Mg shen
+plt.text(10 **15, 1.6, eosName, fontsize=26)
+matplotlib.rc('xtick', labelsize=20)
+matplotlib.rc('ytick', labelsize=20)
+#inset = plt.axes([0.55, 0.205, 0.39, 0.31])  # OTHER
+inset = plt.axes([0.52, 0.205, 0.42, 0.32])  # Mg Shen
+for thePlot in plotList:
+    #print thePlot
+    plt.semilogx(*thePlot[0], c=thePlot[1], ls=thePlot[2])
+#inset.xaxis.set_label_text(fontsize=20)
+plt.xlim([9e14, 2.05e15])
+plt.ylim([2.25, 2.43])  # Mg LS220
+plt.ylim([2.25, 2.77])  # Mb LS220
+plt.ylim([2.51, 3.05])  # Mb Shen
+plt.ylim([2.53, 2.675])  # Mg Shen
 plt.show()
